236e0e3eaba282d8c766d6c16da1aef65edcefb7
gamemanuel <themathnight@gmail.com>
2026-05-29 19:45:42 -0700

    finished the policies section. all i moved here was changing the padding so it was more responsive

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         <Testimonials/>
         <Demos/>
         <AboutMe/>
+        <Policies/>
         <Filler/>
-        {/*<Policies/>*/}
         {/*<Footer/>*/}
     </div>
   );
--- components/Policies.tsx
+++ components/Policies.tsx
@@ -5,7 +5,7 @@ export default function Policies() {
     return (
         <section
             id="policies"
-            className="bg-[#030115] w-full h-full flex flex-col items-center justify-center relative z-0 sm:p-8 md:p-0 "
+            className="bg-[#030115] w-full h-full flex flex-col items-center justify-center relative z-0 p-8"
         >
             <h1 className="pb-8 text-white text-6xl font-bold font-['Inter'] leading-16 [text-shadow:0px_8px_8px_rgb(0_0_0/0.25)]">Policies</h1>
             <Accordion
