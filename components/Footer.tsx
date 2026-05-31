3cc653be896873a5ee67207e15f8dda32c162a70
gamemanuel <themathnight@gmail.com>
2026-05-30 20:49:20 -0700

    added the cameron thrap 2026 text at the bottom of contact

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
       <section
         id="contact"
-        className="bg-[#030115] w-full h-full flex items-center justify-center relative z-0 "
+        className="bg-[#030115] w-full h-full flex flex-col items-center justify-center relative z-0 "
       >
           {/*<div className="flex items-center justify-center w-full bg-indigo-950 rounded-[50px]" >*/}
               {/*<div className="w-full h-[467px] rounded-full relative z-1">*/}
@@ -30,13 +30,19 @@ export default function Footer() {
               {/*    <div className="w-1/20 h-44 left-[1099.46px] top-[148px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
               {/*</div>*/}
 
-          <div className="flex flex-col items-center justify-center space-y-12 bg-[linear-gradient(235.86deg,rgb(0,0,7)_15.356%,rgb(17,16,80)_54.565%,rgb(55,57,186)_87.89%)] w-full h-1/2 lg:w-3/4 lg:h-[467px] rounded-[50px] p-8 md:p-16 mx-2 mb-16 lg:my-32">
+          <div className="flex flex-col items-center justify-center space-y-12 bg-[linear-gradient(235.86deg,rgb(0,0,7)_15.356%,rgb(17,16,80)_54.565%,rgb(55,57,186)_87.89%)] w-full h-1/2 lg:w-3/4 lg:h-[467px] rounded-[50px] p-8 md:p-16 mx-2 mb-10 lg:my-32">
               <h1 className="text-white md:text-6xl font-bold font-['Inter'] text-center leading-16 [text-shadow:0px_8px_8px_rgb(0_0_0/0.25)] text-balance text-4xl ">level up your <span className="text-blue-400">VO</span></h1>
               <Button asChild className="h-16 w-50 rounded-[20px] bg-white text-2xl font-bold text-indigo-950 font-inter hover:text-blue-400">
                   <Link href="mailto:catthrap@gmail.com">
                       Contact Me
                   </Link>
               </Button>
+          </div>
+          <div className="p-8 w-full h-fit ">
+              <h3 className="text-white text-xl font-bold text-center md:text-start ">© Cameron Thrap 2026</h3>
+              <div>
+
+              </div>
           </div>
       </section>
     );
