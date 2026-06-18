699db30f93729db35b27d770ca5e125472633f2d
gamemanuel <themathnight@gmail.com>
2026-06-18 13:03:16 -0700

    added the new footer page and incorpreated font awsome styles to make the images at the bottom more appealing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,35 @@
-import {Button} from "@/components/ui/button";
+
 import Link from "next/link";
+import {Copyright} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faImdb, faInstagram, faFacebook} from "@fortawesome/free-brands-svg-icons";
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
 
 export default function Footer() {
+    const socialLinks: {href:string, icon:IconDefinition, alt:string}[] = [
+        {
+            href: "https://www.imdb.com/name/nm14713806/?ref_=fn_t_1",
+            icon: faImdb,
+            alt: "Link to Internet movie database profile for Cameron Thrap"
+        },
+        {
+            href: "https://www.instagram.com/cameronthrap/",
+            icon: faInstagram,
+            alt: "Link to the Instagram profile for Cameron Thrap"
+        },
+        {
+            href: "https://www.facebook.com/cameron.thrap/",
+            icon: faFacebook,
+            alt: "Link to the Facebook profile for Cameron Thrap"
+        },
+    ]
+
     return (
       <section
         id="contact"
         className="bg-[#030115] w-full h-full flex flex-col items-center justify-center relative z-0 "
       >
-          {/*<div className="flex items-center justify-center w-full bg-indigo-950 rounded-[50px]" >*/}
-              {/*<div className="w-full h-[467px] rounded-full relative z-1">*/}
-              {/*    <div className="w-1/20 h-44 left-0 top-[156px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-
-              {/*</div>*/}
-              {/*<div className=" w-full h-[467px] relative rounded-[100px] z-1">*/}
-              {/*    <div className="w-1/20 h-44 left-0 top-[156px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-64 left-[78.53px] top-25.5 absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-80 left-[157.07px] top-[77px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[235.60px] top-[10px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[314.13px] top-[85px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[392.66px] top-[23px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[471.20px] top-0 absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[549.73px] top-[34px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-80 left-[628.26px] top-[77px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-96 left-[706.79px] top-[34px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-72 left-[785.33px] top-[103px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-80 left-[863.86px] top-[54px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-72 left-[942.39px] top-[85px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-64 left-[1020.92px] top-[116px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*    <div className="w-1/20 h-44 left-[1099.46px] top-[148px] absolute bg-linear-to-b from-indigo-700 from-55% to-white rounded-[100px] to-125%" />*/}
-              {/*</div>*/}
-
           <div className="flex flex-col items-center justify-center space-y-12 bg-[linear-gradient(235.86deg,rgb(0,0,7)_15.356%,rgb(17,16,80)_54.565%,rgb(55,57,186)_87.89%)] w-full h-1/2 lg:w-3/4 lg:h-[467px] rounded-[50px] p-8 md:p-16 mx-2 mb-10 lg:my-32">
               <h1 className="text-white md:text-6xl font-bold font-['Inter'] text-center leading-16 [text-shadow:0px_8px_8px_rgb(0_0_0/0.25)] text-balance text-4xl ">level up your <span className="text-blue-400">VO</span></h1>
               <Button asChild className="h-16 w-50 rounded-[20px] bg-white text-2xl font-bold text-indigo-950 font-inter hover:text-blue-400">
@@ -38,10 +38,23 @@ export default function Footer() {
                   </Link>
               </Button>
           </div>
-          <div className="p-8 w-full h-fit ">
-              <h3 className="text-white text-xl font-bold text-center md:text-start ">© Cameron Thrap 2026</h3>
-              <div>
-
+          <div className="p-8 w-full h-fit flex justify-between">
+              <p className="flex flex-row items-center text-white font-[400] text-[18px]">
+                  <Copyright className="w-5 h-5"/>
+                  <span className="text-white text-xl font-bold text-center md:text-start ml-2">Cameron Thrap 2026</span>
+              </p>
+              {/* Socials: */}
+              <div className="flex space-x-4 pb-4 md:pb-0">
+                  {socialLinks.map((social) => (<a
+                      key={social.alt}
+                      href={social.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={social.alt}
+                      className="transition-transform transform hover:scale-110 hover:opacity-80"
+                  >
+                      <FontAwesomeIcon className="text-white" size="3x" icon={social.icon} />
+                  </a>))}
               </div>
           </div>
       </section>
